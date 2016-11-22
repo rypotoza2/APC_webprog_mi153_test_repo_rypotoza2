d3acc279601d1caf097acfaf6f32d23da1ee1cc5
Rain Potoza <rypotoza2@gmail.com>
2016-11-22 12:41:23 +0800

    edited index.php

--- exercise5/index.php
+++ exercise5/index.php
@@ -8,14 +8,14 @@ if(isset($_GET['delete_id']))
  mysqli_query($con,$sql_query);
  header("Location: $_SERVER[PHP_SELF]");
 }
-// delete condition
+// dcondition deleted
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CRUD Operations With PHP and MySql - By Cleartuts</title>
+<title>CRUD Operations With PHP and MySql - By Cleartuts </title>
 <link rel="stylesheet" href="style.css" type="text/css" />
 <script type="text/javascript">
 function edt_id(id)
