a62a9c9116370b3c040580dd6da7580ba2ace2ab
Rain Potoza <rypotoza2@gmail.com>
2016-11-22 12:23:47 +0800

    edit edit_data.php

--- exercise5/edit_data.php
+++ exercise5/edit_data.php
@@ -8,7 +8,7 @@ if(isset($_GET['edit_id']))
 }
 if(isset($_POST['btn-update']))
 {
- // variables for input data
+ // variables to input a data
  $first_name = $_POST['first_name'];
  $last_name = $_POST['last_name'];
  $city_name = $_POST['city_name'];
