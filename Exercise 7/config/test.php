bb555c03a833cca031160b5e69f75069f476ffbe
Rain Potoza <rypotoza2@gmail.com>
2016-12-22 13:28:08 +0800

    edited test.php

--- Exercise 7/config/test.php
+++ Exercise 7/config/test.php
@@ -3,7 +3,7 @@ $params = require(__DIR__ . '/params.php');
 $dbParams = require(__DIR__ . '/test_db.php');
 
 /**
- * Application configuration shared by all test types
+ * Application configuration shared by all the test types
  */
 return [
     'id' => 'basic-tests',
@@ -28,6 +28,7 @@ return [
             'csrfCookie' => [
                 'domain' => 'localhost',
             ],
+			test.php
             */
         ],        
     ],
